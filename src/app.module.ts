88ead4afa68342f41713ec3ce90307da78002f43
Victor Mafra <victorvmafra97@gmail.com>
2023-08-24 16:07:24 -0300

    feat/CRUD-Prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MediasModule } from './medias/medias.module';
+import { PostsModule } from './posts/posts.module';
+import { PublicationsModule } from './publications/publications.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [MediasModule, PostsModule, PublicationsModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
